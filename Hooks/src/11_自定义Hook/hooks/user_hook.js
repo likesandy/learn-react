fc234fc8c9ea1f738a5606873d513c5142d16da1
sandy <1445709137@qq.com>
2021-05-18 08:51:40 +0800

    learn-hook

--- Hooks/src/11_自定义Hook/hooks/user_hook.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext, UserContext } from "../../App";
-
-export default function useUserContex() {
-  const user = useContext(UserContext);
-  const theme = useContext(ThemeContext);
-  return [user, theme];
-}
